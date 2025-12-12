556fe3f60d90cf65d2118053e910d9aa541a3ec0
kjh015 <khj843@naver.com>
2025-12-12 23:12:05 +0900

    Feat: 초기 세팅 수정, 프롬프트 작성, quiz/pdf 모델 생성

--- lecsum-be/app/core/llm_client.py
+++ lecsum-be/app/core/llm_client.py
@@ -1,1 +1,14 @@
 # GPT-4o-mini 호출 래퍼
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import Runnable
+
+chatOpenAI = ChatOpenAI(
+    temperature=0,
+    model="gpt-4o-mini",
+)
+
+def build_llm_chain(llm, prompt, output) -> Runnable:
+    # 구조화된 출력(Structured Output) 설정
+    structured_llm = llm.with_structured_output(output)
+    chain = prompt | structured_llm
+    return chain
--- lecsum-be/app/services/vector_service.py
+++ lecsum-be/app/services/vector_service.py
@@ -1,1 +1,13 @@
 # vector_service.py
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from typing import List
+
+from core.enums import ChromaDB
+
+
+chroma = Chroma(
+    persist_directory=ChromaDB.PERSIST_DIRECTORY,
+    embedding_function=OpenAIEmbeddings(model="text-embedding-3-small"),
+    collection_name=ChromaDB.COLLECTION_NAME
+)

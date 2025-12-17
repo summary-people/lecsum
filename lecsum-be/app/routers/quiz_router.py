ac1b08c41d3b6313a92897fab08b206cc9a943f3
uni-j-uni <unijun0109@gmail.com>
2025-12-17 11:04:52 +0900

    :recycle:Refactor: import 라인 수정

--- lecsum-be/app/routers/quiz_router.py
+++ lecsum-be/app/routers/quiz_router.py
@@ -2,9 +2,6 @@
 from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from typing import List
-
-
 from app.db.quiz_schemas import (
     QuizRequest,
     QuizResponse,
